c834e908e49e2d3b7742954ad0c74469f42c9b01
Michael Glas <michael.glas93@gmail.com>
2025-03-11 09:46:12 +0100

    Initial Commit

---
+++ cs193p/cs193pApp.swift
@@ -0,0 +1,17 @@
+//
+//  cs193pApp.swift
+//  cs193p
+//
+//  Created by Glas, Michael on 11.03.25.
+//
+
+import SwiftUI
+
+@main
+struct cs193pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
